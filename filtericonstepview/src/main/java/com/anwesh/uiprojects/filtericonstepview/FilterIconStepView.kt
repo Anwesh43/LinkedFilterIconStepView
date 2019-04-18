465c133c997a5346a9ebbe2ee5d1ea49c2dc9b4e
Anwesh43 <anweshthecool0@gmail.com>
2019-04-18 12:38:21 +0530

    adding FISNode class

--- filtericonstepview/src/main/java/com/anwesh/uiprojects/filtericonstepview/FilterIconStepView.kt
+++ filtericonstepview/src/main/java/com/anwesh/uiprojects/filtericonstepview/FilterIconStepView.kt
@@ -126,4 +126,48 @@ class FilterIconStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FISNode(var i : Int, val state : State = State()) {
+
+        private var next : FISNode? = null
+        private var prev : FISNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = FISNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFISNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FISNode {
+            var curr : FISNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
