0a17409964a72b2adbe4b34b0a44f221ac7e23ad
Anwesh43 <anweshthecool0@gmail.com>
2019-04-18 12:42:17 +0530

    adding FilterIconStep class

--- filtericonstepview/src/main/java/com/anwesh/uiprojects/filtericonstepview/FilterIconStepView.kt
+++ filtericonstepview/src/main/java/com/anwesh/uiprojects/filtericonstepview/FilterIconStepView.kt
@@ -170,4 +170,28 @@ class FilterIconStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class FilterIconStep(var i : Int) {
+
+        private val root : FISNode = FISNode(0)
+        private var curr : FISNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
